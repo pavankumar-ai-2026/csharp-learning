578f692da54bd9c189671657eaaca4245accf03c
Pavan Kumar <chpavankumar145@gmail.com>
2026-06-30 14:32:31 +0530

    Day01 : Created first console application

---
+++ Day01/FirstApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+//Console.WriteLine("Hello Pavan");
+//Console.WriteLine("Welcome to Day1");
+//Console.WriteLine("Hello pavan kumar");
+
+//string name = "Pavan";
+//int experience = 4;
+
+//Console.WriteLine($"name: {name}");
+//Console.WriteLine($"Experience: {experience} years");
+
+//string company = "Amplelogic";
+//string role = "developer";
+//int learninghours = 3;
+
+//Console.WriteLine($"Comapne:{company}, Role:{role},Learninghours :{learninghours}");
+
+
+Console.WriteLine("Enter Your Name");
+string name = Console.ReadLine();
+
+Console.WriteLine($"your name is {name}");
+
