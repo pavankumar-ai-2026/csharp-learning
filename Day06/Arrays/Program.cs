176a068bfe4d8fa02e9038153183559c53d95b3f
Pavan Kumar <chpavankumar145@gmail.com>
2026-07-01 23:43:50 +0530

    Dayo6 arrays and day06 notes

---
+++ Day06/Arrays/Program.cs
@@ -0,0 +1,52 @@
+﻿
+//int[] marks =
+//{
+//    90,85,70,60,88
+//};
+
+//foreach (var mark in marks)
+//{
+//    Console.WriteLine(mark);
+//}
+
+//string[] names =
+//{
+//    "pavan",
+//    "kumar",
+//    "raja",
+//    "shreeya",
+//    "mansi"
+//};
+
+//foreach (var name in names)
+//{
+//    Console.WriteLine(name);
+//}
+
+//int[] numbers =
+//{
+//    0,1,2,3,4,5
+//};
+
+//int num = numbers[0];
+
+//for(int i = 0;i < numbers.Length; i++)
+//{
+//    if(numbers[i] > num)
+//    {
+//        num = numbers[i];
+//    }
+//}
+//Console.WriteLine($"Highest number is {num}");
+
+
+string[] weeksdays =
+{
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "thursday",
+    "friday"
+};
+
+Console.WriteLine(weeksdays[2]);
